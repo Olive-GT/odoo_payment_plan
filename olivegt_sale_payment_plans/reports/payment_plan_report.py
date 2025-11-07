0ec5cc10b7fbb0f3533bfdf4db594f035bd4ca6c
JuancaOlive <juanca@olivegt.com>
2025-11-07 09:09:57 -0600

    Revert "Update payment_plan_report.py"
    This reverts commit d3e892e85efd7dd269b260e9b309245ee98c2224.

--- olivegt_sale_payment_plans/reports/payment_plan_report.py
+++ olivegt_sale_payment_plans/reports/payment_plan_report.py
@@ -18,18 +18,3 @@ class PaymentPlanReport(models.AbstractModel):
             'format_date': format_date,
             'formatLang': formatLang,
         }
-
-
-class PaymentPlanReceiptReport(models.AbstractModel):
-    _name = 'report.olivegt_sale_payment_plans.report_payment_plan_reconciliation_receipt'
-    _description = 'Payment Plan Reconciliation Receipt'
-
-    @api.model
-    def _get_report_values(self, docids, data=None):
-        docs = self.env['payment.plan.reconciliation'].browse(docids)
-        return {
-            'doc_ids': docids,
-            'doc_model': 'payment.plan.reconciliation',
-            'docs': docs,
-            'formatLang': formatLang,
-        }
