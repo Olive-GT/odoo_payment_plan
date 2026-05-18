0257df843e6e7d897bba6725926cd648e3e9c372
JuancaOlive <juanca@olivegt.com>
2026-05-18 14:11:57 -0600

    changes

--- olivegt_sale_payment_plans/models/company.py
+++ olivegt_sale_payment_plans/models/company.py
@@ -6,5 +6,28 @@ class ResCompany(models.Model):
 
     payment_plan_receipt_bg_url = fields.Char(
         string='Payment Plan Receipt Background URL',
+        compute='_compute_payment_plan_receipt_bg_url',
+        inverse='_inverse_payment_plan_receipt_bg_url',
         help='Background image URL used in the payment plan allocation receipt PDF.',
     )
+
+    def _get_payment_plan_receipt_bg_url_key(self):
+        self.ensure_one()
+        return f'olivegt_sale_payment_plans.payment_plan_receipt_bg_url.{self.id}'
+
+    def _compute_payment_plan_receipt_bg_url(self):
+        config = self.env['ir.config_parameter'].sudo()
+        for company in self:
+            company.payment_plan_receipt_bg_url = config.get_param(
+                company._get_payment_plan_receipt_bg_url_key(),
+                default='',
+            )
+
+    def _inverse_payment_plan_receipt_bg_url(self):
+        config = self.env['ir.config_parameter'].sudo()
+        for company in self:
+            key = company._get_payment_plan_receipt_bg_url_key()
+            if company.payment_plan_receipt_bg_url:
+                config.set_param(key, company.payment_plan_receipt_bg_url)
+            else:
+                config.search([('key', '=', key)]).unlink()
